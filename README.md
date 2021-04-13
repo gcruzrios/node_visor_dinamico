689cbb7887abb246de3502be53c179cb876afb9a
Daniel Flores <danielflorescr@gmail.com>
2021-04-13 15:28:46 -0600

    Prueba de COncepto: Visor dinámico de capas

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# NODE VISOR SINIA
+Visor SINIA
 
-Visor dinamico integrado a geoserver
+# Descripcion
+
+Implementación de un visor web que obtiene dinámicamente las capas de un proveedor de información geoespacial, por ejemplo geoserver.
+
+# Instrucciones
+
+```
+npm install
+npm run-script build
+                 'views/index.html' -> 'dist/index.html'
+node index.js
+http://localhost:3000/demo/
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+const proxy = require('express-http-proxy')
+const express = require('express')
+const app = express()
+const url = require('url')
+const path = require('path')
+ 
+const geoserverProxy = {
+  forwardPath: (req, res) => {
+    return url.parse(req.originalUrl).path
+  }
+}
+
+app.use('/geoserver/wms', proxy('http://18.218.14.134:8080/geoserver/CENIGA/wms', geoserverProxy))
+app.use('/geoserver/wfs', proxy('http://18.218.14.134:8080/geoserver/CENIGA/wfs', geoserverProxy))
+
+app.use('/demo/static', express.static('static'))
+
+app.get('/demo/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+app.listen(3000, function () {
+  console.log('app listening on port 3000!');
+})
